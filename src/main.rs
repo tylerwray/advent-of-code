2a46a5b1cbdee91e908988568202ed8937ba9c2d
Tyler Wray <wraytw@gmail.com>
2021-01-19 20:57:53 -0700

    IDK

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 mod day_one;
+mod day_three;
 mod day_two;
 
 fn main() {
     day_one::check_module_mass();
     day_one::check_fuel_mass();
+
     let position_zero = day_two::intcode_computer(12, 2);
     println!("Position 0 -> {}", position_zero);
+
     let code = day_two::gravity_assist();
-    println!("Code -> {}", code)
+    println!("Code -> {}", code);
+
+    let distance = day_three::distance_to_closest_crossing_point();
+    println!("Distance -> {}", distance);
 }
